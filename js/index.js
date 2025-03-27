30bb22ff6954a6bfa514d9a4599972127d14b07d
soydex <johndoe@example.com>
2025-03-27 10:54:42 +0100

    Ajout d'une section héro avec un carrousel d'images, mise à jour des liens d'icônes, et amélioration des styles CSS pour une meilleure expérience utilisateur.

--- js/index.js
+++ js/index.js
@@ -13,3 +13,28 @@ questionBoxes.forEach((box) => {
     });
   }
 });
+
+const heroSection = document.querySelector('.hero');
+const dots = document.querySelectorAll('.dot');
+
+const heroBackgrounds = [
+  'hero hero-1', 
+  'hero hero-2', 
+  'hero hero-3', 
+];
+
+dots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    dots.forEach((dot) => dot.classList.remove('active'));
+    heroSection.className = heroBackgrounds[index] || heroBackgrounds[0];
+    dot.classList.add('active');
+  });
+});
+
+let currentIndex = 0;
+setInterval(() => {
+  dots.forEach((dot) => dot.classList.remove('active'));
+  currentIndex = (currentIndex + 1) % heroBackgrounds.length;
+  heroSection.className = heroBackgrounds[currentIndex];
+  dots[currentIndex].classList.add('active');
+}, 7500);
