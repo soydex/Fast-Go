89cc6ed6b01eb335878245b501f56253732f6c2c
soydex <johndoe@example.com>
2025-04-25 09:54:51 +0200

    Mise à jour des métadonnées et amélioration de la structure des fichiers HTML, CSS et JavaScript pour une meilleure gestion des véhicules et une expérience utilisateur optimisée.

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,10 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const menu = document.querySelector('#menu');
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('#menu');
 
-menuToggle.addEventListener('click', () => {
-    const isOpen = menuToggle.classList.contains('open');
-    menuToggle.classList.toggle('open', !isOpen);
-    menu.classList.toggle('show', !isOpen);
+    menuToggle.addEventListener('click', () => {
+        const isOpen = menuToggle.classList.contains('open');
+        menuToggle.classList.toggle('open', !isOpen);
+        menu.classList.toggle('show', !isOpen);
+    });
 });
